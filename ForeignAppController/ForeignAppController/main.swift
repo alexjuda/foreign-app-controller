e0ae376c6c651602643d056f09282ac9639ec392
Alexander Juda <alexander.juda@icloud.com>
2019-01-20 12:42:25 +0100

    Add empty command line target

---
+++ ForeignAppController/ForeignAppController/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ForeignAppController
+//
+//  Created by Alexander Juda on 20/01/2019.
+//  Copyright © 2019 Brand Amandli. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
